46c5cb5bbcb7b8ad52b23ea1e85a1d67dbd3f7da
Kevin Croke <k.croke@shipworks.com>
2022-07-11 23:35:55 -0500

    Deselect suggestions drop down as to not accidently enter value twice

--- src/Suggestions.svelte
+++ src/Suggestions.svelte
@@ -2,6 +2,8 @@
   import { possibilities, letters } from "./stores";
   let selectedSuggestion: string;
 
+  possibilities.subscribe((x) => (selectedSuggestion = x[0]));
+
   function selectionChanged(event: Event) {
     letters.useSuggestion(selectedSuggestion);
     (event.target as HTMLSelectElement).blur();
